a56f425a50ede6e6e9b0eeec6e988d110471a4cb
Nebil Muren <nebilmuren@gmail.com>
2019-12-02 16:27:22 -0500

    clean addition

--- Add two numbers as a linked list/src/add/two/numbers/as/a/linked/list/AddTwoNumbersAsALinkedList.java
+++ Add two numbers as a linked list/src/add/two/numbers/as/a/linked/list/AddTwoNumbersAsALinkedList.java
@@ -1,62 +1,80 @@
 package add.two.numbers.as.a.linked.list;
 
 import java.util.LinkedList;
-
-class Node {
-
-    private int data;
-    private Node next;
-
-    Node(int data) {
-        this.data = data;
-    }
-
-    int getData() {
-        return data;
-    }
-
-    Node getNext() {
-        return next;
-    }
-}
+import java.util.Scanner;
 
 public class AddTwoNumbersAsALinkedList {
 
+    static LinkedList generateList(Integer n) {
+        LinkedList list = new LinkedList();
+        while (n > 0) {
+            list.add(n % 10);
+            n = Math.floorDiv(n, 10);
+        }
+        return list;
+    }
+
+    static LinkedList addNumbers(LinkedList l1, LinkedList l2) {
+        LinkedList results = new LinkedList();
+        int size = Math.max(l1.size(), l2.size());
+        int circulatingValue = 0;
+        while (size-- > 0) {
+            int sum = circulatingValue;
+            circulatingValue = 0;
+
+            if (!l1.isEmpty()) {
+                sum += ((Integer) l1.poll());
+            }
+            if (!l2.isEmpty()) {
+                sum += ((Integer) l2.poll());
+            }
+            if (sum > 9) {
+                results.add(sum % 10);
+                circulatingValue = 1;
+            } else {
+                results.add(sum);
+            }
+        }
+        if (circulatingValue != 0) {
+            results.add(circulatingValue);
+        }
+        return results;
+    }
+
+    static void printList(LinkedList list) {
+        StringBuilder output = new StringBuilder();
+        while (!list.isEmpty()) {
+            output.append(list.poll());
+            if (list.size() > 0) {
+                output.append(" -> ");
+            }
+        }
+        System.out.println(output);
+    }
+
     public static void main(String[] args) {
-        LinkedList l1 = new LinkedList();
-        l1.add(new Node(2));
-        l1.add(new Node(4));
-        l1.add(new Node(3));
-        LinkedList l2 = new LinkedList();
-        l2.add(new Node(5));
-        l2.add(new Node(6));
-        l2.add(new Node(4));
+        Scanner input = new Scanner(System.in);
+        try {
+            LinkedList l1 = generateList(input.nextInt());
+            LinkedList l2 = generateList(input.nextInt());
+            /*LinkedList l1 = new LinkedList();
+            l1.add(2);
+            l1.add(4);
+            l1.add(3);
+            LinkedList l2 = new LinkedList();
+            l2.add(5);
+            l2.add(6);
+            l2.add(4);*/
 
-        LinkedList lResult = new LinkedList();
-        int l1Value = 0;
-        int l2Value = 0;
-        int size = l1.size();
-        for (int i = 0; i < size; i++) {
-            l1Value += ((Node) l1.poll()).getData() * Math.pow(10, i);
-            l2Value += ((Node) l2.poll()).getData() * Math.pow(10, i);
+            LinkedList resultList = addNumbers(l1, l2);
+            printList(resultList);
 
+        } catch (Exception e) {
+            System.out.println("I had a problem preparing your lists!\n"
+                    + "Try entering 'xxx yyy' or \n'xxx\nyyy'");
         }
+        input.close();
 
-        Integer sum = l1Value + l2Value;
-        //System.out.println(sum);
-
-        String s = sum.toString();
-        char[] c = s.toCharArray();
-
-        for (int i = c.length - 1; i >= 0; i--) {
-            char c1 = c[i];
-            lResult.add(new Node(Character.getNumericValue(c1)));
-        }
-        int size2 = lResult.size();
-        for (int i = 0; i < size2; i++) {
-            Node n3 = (Node) lResult.poll();
-            System.out.println(n3.getData());
-        }
     }
 
 }
